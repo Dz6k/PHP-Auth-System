165d039381e43ad644875a04a5bb8723ef19589d
Pedro Souza <pedrosouzadz6@pm.me>
2025-08-03 13:02:59 -0300

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#31251a",
+        secondary: "#291f16",
+      },
+    },
+  },
+  plugins: [],
+};
