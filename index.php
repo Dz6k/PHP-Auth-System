fecfbeee60659e3b6d1a867cf503e85abc1eded9
Pedro Souza <pedrosouzadz6@pm.me>
2025-08-03 13:05:57 -0300

    add html5 + tailwind template

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php
-
-echo 'First commit';
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    
+    <title>Auth System</title>
+</head>
+<body>
+</body>
+</html>
